3f80dcb6f04494850a12e9de34862c9a9d4181f8
Billy <billybdev@gmail.com>
2022-11-05 13:05:12 -0700

    initialize repo for java playground

---
+++ src/ArrList.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+public class ArrList {
+        public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+        // ListArray
+        ArrayList<String> myArr = new ArrayList<>();
+
+        myArr.add("kitty");
+        myArr.add("kitty");
+        myArr.add("kitty");
+
+        // System.out.println(myArr.remove(1));
+        // System.out.println(myArr);
+
+        print(myArr);
+        System.out.println(myArr.size());
+
+
+    }
+
+    public static void print(ArrayList<String> list){
+
+        // list = new ArrayList<>();
+        for(String str: list) System.out.println(str);
+        // for (int i = 0; i < list.size(); i++) System.out.println(list[i]);
+    }
+}
